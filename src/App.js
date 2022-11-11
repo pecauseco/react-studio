d7b9d88ac06960601b718d076a07f2ece2aa07f4
Charles Levy <charleslevy@MacBook-Air-207.local>
2022-11-10 19:11:41 -0500

    yass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,27 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState([]);
+  const [totPrice, setTotPrice] = useState(0);
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        //<p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem name={item.name} price={item.price} image= {item.image} cart = {cart} setCart={setCart} 
+        totPrice = {totPrice} setTotPrice = {setTotPrice}/>
       ))}
 
       <div>
         <h2>Cart</h2>
+        <br/>
         {/* TODO: render a list of items in the cart */}
+        <div>{cart}</div>
+        <div>{totPrice}</div>
+        
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,28 @@
 // TODO: create a component that displays a single bakery item
+import { useState } from 'react';
+
+export default function BakeryItem(props){
+    const totPrice = props.totPrice;
+    const setTotPrice = props.setTotPrice;
+const name = props.name;
+const price = props.price;
+const image = props.image;
+const cart = props.cart;
+const setCart = props.setCart;
+const total = props.total;
+const setTotal = props.setTotal;
+const [index, setIndex] = useState(0);
+function handleNextClick() {
+    setTotPrice(totPrice + price);
+    setCart([...cart, name, " ", price, " ", <br/>]);
+    
+    setIndex(index + 1);
+  }
+    return (
+        <div>
+        <div>{name} {price} </div>
+        <img src = {image}/>
+        <button onClick={handleNextClick}>Add to cart</button>
+        </div>
+    )
+}
